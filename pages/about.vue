8a575e8eded29012d76799ceea28bfcce3a0ded2
Raul Morais <raulmorais@tec-adm.com.br>
2024-05-16 16:37:28 -0300

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h1>about</h1>
+</div>
+</template>
+
+
+<script lang="ts">
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>index</h1>
+        <AppAlert>
+            This iskjkio an auto-imported component.
+        </AppAlert>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
+</template>
+    
+    
+<script lang="ts">
+
+</script>
+    
+    
+<style scoped>
+
+</style>
